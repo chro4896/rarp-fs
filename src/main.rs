17a6f3abb1e0d35e59cd78015f7c9da4b1a99975
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-07-09 07:23:05 +0900

    modify prototype of fuse_main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[link(name = "fuse3")]
 extern {
-	fn fuse_main_real(argc: core::ffi::c_int, argv: **mut core::ffi::c_char, op: *const fuse_operations, op_size: usize, private_data: *mut core::ffi::c_void);
+	fn fuse_main_real(argc: core::ffi::c_int, argv: *mut *mut core::ffi::c_char, op: *const fuse_operations, op_size: usize, private_data: *mut core::ffi::c_void);
 }
 
 #[repr(C)]
