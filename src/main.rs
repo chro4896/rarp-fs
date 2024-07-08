6d9f5f3b49e47c7a557007efa0880396fa3fbf99
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-07-09 06:55:07 +0900

    create read_test_fuse

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,15 @@ pub extern "C" fn open_test_fuse(_path: *const core::ffi::c_char, _fi: *mut core
 	println!("open");
 	0
 }
+
+pub extern "C" fn read_test_fuse(_path: *const core::ffi::c_char, buf: *mut core::ffi::c_char, size: usize, offset: libc::off_t, _fi: *mut core::ffi::c_void) -> core::ffi::c_int {
+	println!("read");
+	if size < 1 || offset > 0 {
+        0
+	} else {
+		unsafe {
+			buf.offset(0) = 10;
+		}
+		1
+	}
+}
