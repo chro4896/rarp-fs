a3e867d85df5475fad8758ff4114eddde0c8b3c3
chro4896 <11955521+chro4896@users.noreply.github.com>
2024-07-18 19:08:16 +0900

    modify function getattr_test_fuse

--- src/main.rs
+++ src/main.rs
@@ -84,10 +84,13 @@ pub extern "C" fn getattr_test_fuse(path: *const core::ffi::c_char, stbuf: *mut 
 	if unsafe { *(path.offset(1)) } == b'\0' as core::ffi::c_char {
 		stbuf.st_mode = libc::S_IFDIR | 0o775;
 		stbuf.st_nlink = 2;
-	} else {
+		0
+	} else if unsafe { *(path.offset(1)) } == b't' && unsafe { *(path.offset(2)) } == b'e' && unsafe { *(path.offset(3)) } == b's' && unsafe { *(path.offset(4)) } == b't' {
 		stbuf.st_mode = libc::S_IFREG | 0o664;
 		stbuf.st_nlink = 1;
 		stbuf.st_size = 1;
+		0
+	} else {
+		(-1)*libc::ENOENT
 	}
-	0
 }
